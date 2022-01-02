8a31f2233e7f25f53ccb147a92a66f4c058b4f8e
Anders Christiansen Sørby <anders@sorby.xyz>
2022-01-02 21:39:12 +0100

    Add naersk nix flake build

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "Lean tree sitter";
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs;
+    flake-utils = {
+      url = github:numtide/flake-utils;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = github:nix-community/naersk;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils = {
+      url = github:yatima-inc/nix-utils;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.naersk.follows = "naersk";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , utils
+    , naersk
+    }:
+    utils.lib.eachDefaultSystem (system:
+    let
+      # Contains nixpkgs.lib, flake-utils.lib and custom functions
+      lib = utils.lib.${system};
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (lib) buildRustProject testRustProject getRust filterRustProject;
+      # Load a nightly rust. The hash takes precedence over the date so remember to set it to
+      # something like `lib.fakeSha256` when changing the date.
+      rustNightly = getRust { date = "2021-12-01"; sha256 = "DhIP1w63/hMbWlgElJGBumEK/ExFWCdLaeBV5F8uWHc="; };
+      crateName = "tree-sitter-lean";
+      root = ./.;
+      # This is a wrapper around naersk build
+      # Remember to add Cargo.lock to git for naersk to work
+      project = buildRustProject {
+        rust = rustNightly;
+        inherit root;
+      };
+    in
+    {
+      packages.${crateName} = project;
+      checks.${crateName} = testRustProject { inherit root; };
+
+      defaultPackage = self.packages.${system}.${crateName};
+
+      # To run with `nix run`
+      apps.${crateName} = flake-utils.lib.mkApp {
+        drv = project;
+      };
+
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        inputsFrom = builtins.attrValues self.packages.${system};
+        nativeBuildInputs = [ rustNightly ];
+        buildInputs = with pkgs; [
+          rust-analyzer
+          clippy
+          rustfmt
+        ];
+      };
+    });
+}
